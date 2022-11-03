946a81a4aa0326d23216d0061b3decd3630ab22a
Tomasz Nowak <tomasz.nowak@mycit.ie>
2022-11-03 19:05:46 +0000

    DOM objects cached addEventListeners added.

---
+++ app.js
@@ -0,0 +1,19 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+// declering and initialing scores
+var scoreUser = 0;
+var scoreComp = 0;
+
+// adding eventListeners to the buttons to starts a round of game
+if(rock){
+    rock.addEventListener('click', () => round("rock"));
+}
+if(paper){
+    paper.addEventListener('click',() => round("paper"));
+}
+if(scissors){
+    scissors.addEventListener('click',() => round("scissors"));
+
+}
